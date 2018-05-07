4f834397c12978346bd98980f6889e2d9eef817c
whyjb <jb.jeong07@gmail.com>
2018-05-07 19:53:51 +0900

    greedy

--- dijkstra.py
+++ dijkstra.py
@@ -6,12 +6,11 @@ int_inf = 987654321
 n = 5
 W = np.array(
   [
-    [int_inf, int_inf, int_inf, int_inf, int_inf, int_inf],
-    [int_inf, int_inf, 7, 4, 6, 1],
-    [int_inf, int_inf, int_inf, int_inf, int_inf, int_inf],
-    [int_inf, int_inf, 2, int_inf, 4, int_inf],
-    [int_inf, int_inf, 3, int_inf, int_inf, int_inf],
-    [int_inf, int_inf, int_inf, int_inf, 1, int_inf]
+    [int_inf, 7, 4, 6, 1],
+    [int_inf, int_inf, int_inf, int_inf, int_inf],
+    [int_inf, 2, int_inf, 4, int_inf],
+    [int_inf, 3, int_inf, int_inf, int_inf],
+    [int_inf, int_inf, int_inf, 1, int_inf]
   ], dtype=np.int32)
   
 def dijkstra():
@@ -19,20 +18,19 @@ def dijkstra():
   nearest = np.zeros(n, dtype=np.int32)
   shortest_distance = np.zeros(n, dtype=np.int32)
   F = set()
-  pdb.set_trace()
   
   # Initialize length
-  length[1] = -1
-  for i in range(2, n+1):
-    length[i] = W[1][i]
-    nearest[i] = 1
+  length[0] = -1
+  for i in range(1, n):
+    length[i] = W[0][i]
+    nearest[i] = 0
     
   for i in range(n-1):
     # Find shortest vertex.
     vnear = None
     min_length = int_inf
-    for v_index in range(2, n+1):
-      if length >= 0 and length[i] < min_length:
+    for v_index in range(1, n):
+      if length[v_index] >= 0 and length[v_index] < min_length:
         min_length = length[v_index]
         vnear = v_index
     
@@ -41,14 +39,14 @@ def dijkstra():
     F.add((nearest[vnear], vnear))
     shortest_distance[vnear] = min_length
     
-    for v_index in range(2, n+1):
+    for v_index in range(1, n):
       if length[vnear] + W[vnear][v_index] < length[v_index]:
         length[v_index] = length[vnear] + W[vnear][v_index]
         nearest[v_index] = vnear
     length[vnear] = -1
     
-    print(F)
-    print(shortest_distance)
+  print(F)
+  print(shortest_distance)
     
     
 if __name__=='__main__':
--- huffman.py
+++ huffman.py
@@ -2,35 +2,37 @@ import heapq
 import numpy as np
 from collections import defaultdict
 
-    
+import pdb
+
 def encode(frequency):
-  heap = [ [weight, [symbol, '']] for weight, symbol in frequency.items() ]
-  print(heap)
+  heap = [ [weight, [symbol, '']] for symbol, weight in frequency.items() ]
   heapq.heapify(heap)
   while len(heap) > 1:
     lo = heapq.heappop(heap)
     hi = heapq.heappop(heap)
-    print(heap)
-    print(type(lo[1]))
-    lo[1][1] = '0' + lo[1][1]
-    hi[1][1] = '1' + hi[1][1]
+
+    for pair in lo[1:]:
+      pair[1] = '0' + pair[1]
+    for pair in hi[1:]:
+      pair[1] = '1' + pair[1]
+
+    lo[0] = lo[0] + hi[0]
+    for pair in hi[1:]:
+        lo.append(pair)
+    heapq.heappush(heap, lo)
+
+  return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1])))
     
-    #for pair in lo[1]:
-    #  pair[1] = '0' + pair[1]
-    #for pair in hi[1]:
-    #  pair[1] = '1' + pair[1]
-      
-    print('hello')
-    heapq.heappush(heap, [lo[0] + hi[0], lo[1] + hi[1]])
-    
-data = "The frog at the bottom of the well drifts off into the great ocean"
-frequency = defaultdict(int)
+if __name__=='__main__':
 
-for symbol in data:
-  frequency[symbol] += 1
+    data = "The frog at the bottom of the well drifts off into the great ocean"
+    frequency = defaultdict(int)
+    print(frequency)
 
-print("frequency: ", frequency)
-huff = encode(frequency)
+    for symbol in data:
+      frequency[symbol] += 1
 
-
-#if __name__=='__main__':
+    huff = encode(frequency)
+    print("Symbol".ljust(10) + "Weight".ljust(10) + "Huffman Code")
+    for p in huff:
+        print(p[0].ljust(10) + str(frequency[p[0]]).ljust(10) + p[1])
