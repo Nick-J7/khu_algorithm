b14387e88e8d442d1bd4d00b6515285216abf461
whyjb <jb.jeong07@gmail.com>
2018-06-07 22:42:03 +0900

    Add make_heap2

--- 0607_heapsort.py
+++ 0607_heapsort.py
@@ -11,7 +11,8 @@ class Heap:
         self.data = list(data)
         self.num_of_data = len(data)
         self.tree = [0]
-        self.make_heap()
+        #self.make_heap()
+        self.make_heap_second()
 
     def heap_sort(self):
         tmp_tree = list(self.tree)
@@ -24,6 +25,12 @@ class Heap:
     def make_heap(self):
         for value in self.data:
             self.add_element(value)
+
+    def make_heap_second(self):
+        for value in self.data:
+            self.tree.append(value)
+        for i in range(len(self.tree), 0, -1):
+            self.sift_down_for_make_heap(i)
 
     def add_element(self, value):
         self.tree.append(value)
@@ -53,16 +60,21 @@ class Heap:
     def sift_down(self, tree):
         last_index = len(tree) - 1
 
-        parent_index = 1
+        parent_index = 1 
         left_child_index = 2 * parent_index
         right_child_index = 2 * parent_index + 1
-        if left_child_index > last_index or right_child_index > last_index:
+        if left_child_index > last_index:
             return
 
-        parent_value = tree[parent_index]
-        left_child_value = tree[left_child_index]
-        right_child_value = tree[right_child_index]
-        max_child_value = max(left_child_value, right_child_value)
+        if right_child_index <= last_index:
+            parent_value = tree[parent_index]
+            left_child_value = tree[left_child_index]
+            right_child_value = tree[right_child_index]
+            max_child_value = max(left_child_value, right_child_value)
+        else:
+            parent_value = tree[parent_index]
+            left_child_value = tree[left_child_index]
+            max_child_value = left_child_value
 
         while parent_value < max_child_value:
             if max_child_value == left_child_value:
@@ -76,14 +88,65 @@ class Heap:
 
             left_child_index = 2 * parent_index
             right_child_index = 2 * parent_index + 1
-            if left_child_index > last_index or right_child_index > last_index:
+
+            if left_child_index > last_index:
                 break
 
-            parent_value = tree[parent_index]
-            left_child_value = tree[left_child_index]
-            right_child_value = tree[right_child_index]
-            max_child_value = max(left_child_value, right_child_value)
+            if right_child_index <= last_index:
+                parent_value = tree[parent_index]
+                left_child_value = tree[left_child_index]
+                right_child_value = tree[right_child_index]
+                max_child_value = max(left_child_value, right_child_value)
+            else:
+                parent_value = tree[parent_index]
+                left_child_value = tree[left_child_index]
+                max_child_value = left_child_value
 
+    def sift_down_for_make_heap(self, root):
+
+        last_index = len(self.tree) - 1
+
+        parent_index = root 
+        left_child_index = 2 * parent_index
+        right_child_index = 2 * parent_index + 1
+        if left_child_index > last_index:
+            return
+
+        if right_child_index <= last_index:
+            parent_value = self.tree[parent_index]
+            left_child_value = self.tree[left_child_index]
+            right_child_value = self.tree[right_child_index]
+            max_child_value = max(left_child_value, right_child_value)
+        else:
+            parent_value = self.tree[parent_index]
+            left_child_value = self.tree[left_child_index]
+            max_child_value = left_child_value
+
+        while parent_value < max_child_value:
+            if max_child_value == left_child_value:
+                self.tree[parent_index] = left_child_value
+                self.tree[left_child_index] = parent_value
+                parent_index = left_child_index
+            else:
+                self.tree[parent_index] = right_child_value
+                self.tree[right_child_index] = parent_value
+                parent_index = right_child_index
+
+            left_child_index = 2 * parent_index
+            right_child_index = 2 * parent_index + 1
+
+            if left_child_index > last_index:
+                break
+
+            if right_child_index <= last_index:
+                parent_value = self.tree[parent_index]
+                left_child_value = self.tree[left_child_index]
+                right_child_value = self.tree[right_child_index]
+                max_child_value = max(left_child_value, right_child_value)
+            else:
+                parent_value = self.tree[parent_index]
+                left_child_value = self.tree[left_child_index]
+                max_child_value = left_child_value
 
 init_list = [11, 14, 2, 7, 6, 3, 9, 5]
 heap = Heap(init_list)
--- hamiltonian.py
+++ hamiltonian.py
@@ -1,7 +1,30 @@
+# -*- coding: utf-8 -*-
 
+n = 5
+W=[[0,1,1,0,1],[1,0,1,1,1],[1,1,0,1,0],[0,1,1,0,1],[1,1,0,1,0]]
 
-if __name__ == '__main__':
-    n = 5
-    vindex = n * [0]
-    W=[[0,1,1,0,1],[1,0,1,1,1],[1,1,0,1,0],[0,1,1,0,1],[1,1,0,1,0]]
+def hamilton(index):
+    #print(vindex)
+    if promising(index):
+        if index == n-1:
+            print(vindex)
+        else:
+            for i in range(1, n):
+                vindex[index + 1] = i
+                hamilton(index + 1)
 
+def promising(index):
+#    if index == n-1:
+#        print(index, vindex[index])
+    if index == n-1 and W[vindex[index]][0] == 0:
+        return False
+    elif index != 0 and W[vindex[index]][vindex[index - 1]] == 0:
+        return False
+    for i in range(1, index):
+        if vindex[i] == vindex[index]:
+            return False
+    return True
+
+     
+vindex = n * [0]
+hamilton(0)
